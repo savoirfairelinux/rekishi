ebf142e46328ffa94fe382373591db1c8ece1c3d
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2014-08-04 08:30:59 -0400

    Added InfluxDB connection info settings to settings.py

--- rekishi/rekishi/settings.py
+++ rekishi/rekishi/settings.py
@@ -80,3 +80,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+## NON-DJANGO CONFIGS
+# Connection info for InfluxDB
+INFLUXDB_URL = 'localhost'
+INFLUXDB_PORT = 8086
+
