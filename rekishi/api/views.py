3572be40f4d8f917a75bc2122d8aecb64a92dba2
Philippe Pepos Petitclerc <ppepos@savoirfairelinux.com>
2014-08-13 14:53:57 -0400

    Some more work

--- rekishi/api/views.py
+++ rekishi/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from . import db
 from . import influxdb_dataset, influxdb_series
 
@@ -37,3 +37,23 @@ def simple_series(request, host=None, service=None, series=None):
     #     'data': ret_value,
     #     'query': query
     # })
+
+def host_services(request, host=None):
+
+    try:
+        query = 'SELECT * from /%s\..*/ limit 1;' % host
+
+        data = db.query(query)
+        services = []
+        for service in data:
+            services.append(service['name'])
+        # data = influxdb_dataset(data)
+
+        ret_value = json.dumps(services, indent=2)
+    except:
+        raise Http404
+
+    return HttpResponse(ret_value, content_type="application/json")
+
+def service_series(request, host=None, service=None):
+    pass
