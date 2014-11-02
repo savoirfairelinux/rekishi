8718c68cc2416ce126ecdf125dd77208d51812c1
Thibault Cohen <titilambert@gmail.com>
2014-11-02 05:13:21 -0500

    Accept .* as host to get all hosts

--- rekishi/api/urls.py
+++ rekishi/api/urls.py
@@ -4,7 +4,7 @@ from rekishi.api import views
 
 urlpatterns = patterns('', 
     # url(r'^bypass/', views.bypass, name='bypass'),
-    url(r'dg/(?P<host>[A-Za-z0-9._]+)/$',
+    url(r'dg/(?P<host>[A-Za-z0-9._*]+)/$',
            views.dg_host_series, name='dg_host_series'),
     url(r'dg/(?P<host>[A-Za-z0-9._]+)/(?P<service>[A-Za-z0-9._]+)/$',
             views.dg_service_series, name='dg_service_series'),
